13b3316d063e98aab425f9e270a45e9ef61fbbdc
박은경 <dmsrud1501222@naver.com>
2023-03-06 11:30:11 +0900

    20230305

--- DFSBFS/B_1260.java
+++ DFSBFS/B_1260.java
@@ -1,74 +1,82 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class B_1260 {
 
-	static StringBuilder sb = new StringBuilder();
-	static boolean[] check;
+public class B_1260{
+
+
+	static int N;
+	static int M;
+	static int V;
 	static int[][] arr;
-	
-	static int node, line, start;
-	
-	static Queue<Integer> q = new LinkedList<>();
+	static boolean[] visit;
+	static StringBuilder sb = new StringBuilder();
+	static Queue<Integer> Q = new LinkedList<>();
 
-	public static void main(String[] args) throws IOException {
-		
+	public static void main(String[] args)throws IOException{
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		node = Integer.parseInt(st.nextToken());
-		line = Integer.parseInt(st.nextToken());
-		start= Integer.parseInt(st.nextToken());
-		
-		arr = new int[node+1][node+1];
-		check = new boolean[node+1];
-		
-		for(int i = 0 ; i < line ; i ++) {
-			StringTokenizer str = new StringTokenizer(br.readLine());
-			
-			int a = Integer.parseInt(str.nextToken());
-			int b = Integer.parseInt(str.nextToken());
-			
-			arr[a][b] = arr[b][a] =  1;	
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		V = Integer.parseInt(st.nextToken());
+
+		arr = new int[N + 1][N + 1];
+		visit = new boolean[N + 1];
+;		for(int i = 0 ; i < M; i ++){
+
+			String str2 = br.readLine();
+			StringTokenizer st2 = new StringTokenizer(str2, " ");
+
+			int a = Integer.parseInt(st2.nextToken());
+			int b = Integer.parseInt(st2.nextToken());
+
+			arr[a][b] = arr[b][a] = 1;
 		}
-			//sb.append("\n");
-			dfs(start);
-			sb.append("\n");
-			check = new boolean[node+1];
-			
-			bfs(start);
-			
-            System.out.println(Arrays.deepToString(arr));
-			System.out.println(sb);
-		
-		}
-	public static void dfs(int start) {
-		
-		check[start] = true;
-		sb.append(start + " ");
-		
-		for(int i = 0 ; i <= node ; i++) {
-			if(arr[start][i] == 1 && !check[i])
-				dfs(i);
-		}
-		
+
+		dfs(V);
+		sb.append("\n");
+
+		visit = new boolean[N + 1];
+
+		bfs(V);
+		System.out.println(sb);
+
 	}
-	
-	public static void bfs(int start) {
-		q.add(start);
-		check[start] = true;
+
+	public static void dfs(int start){
+
+		visit[start] = true;
+		sb.append(start +  " ");
+
+		for(int i = 1 ; i <= N; i ++){
+
+			if(arr[start][i] == 1 && !visit[i]){
+				visit[i] = true;
+				dfs(i);
+			}
+		}
+	}
+
+	public static void bfs(int start){
+
+		Q.offer(start);
+		visit[start] = true;
 		
-		while(!q.isEmpty()) {
-			
-			start = q.poll();
-			sb.append(start + " ");
-			
-			for(int i = 1 ; i <= node ; i++) {
-				if(arr[start][i] == 1 && !check[i]) {
-					q.add(i);
-					check[i] = true;
+		while(!Q.isEmpty()){
+
+			int n = Q.poll();
+			sb.append(n + " ");
+
+			for(int i = 1; i <= N; i ++){
+				if(arr[n][i] == 1 && !visit[i]){
+					Q.offer(i);
+					visit[i] = true;
 				}
 			}
+
 		}
 	}
 }
--- DFSBFS/B_2178.java
+++ DFSBFS/B_2178.java
@@ -1,66 +1,3 @@
-// import java.io.*;
-// import java.util.*;
-
-// public class B_2178 {
-	
-// 	static int[][] map;
-// 	static int n;
-// 	static int m;
-// 	static boolean[][] visited;
-// 	static int[] dx = { -1, 1, 0, 0 }; //x방향배열-상하
-//  static int[] dy = { 0, 0, -1, 1 }; //y방향배열-좌우
-
-// 	public static void main(String[] args) throws IOException {
-// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-// 		StringTokenizer st = new StringTokenizer(br.readLine());
-// 		n = Integer.parseInt(st.nextToken());
-// 		m = Integer.parseInt(st.nextToken());
-		
-// 		map = new int[n][m];
-// 		for(int i=0; i<n; i++) {
-// 			String s = br.readLine();
-// 			for(int j=0; j<m; j++) {
-// 				map[i][j] = s.charAt(j) - '0';
-// 			}
-// 		}
-		
-// 		visited = new boolean[n][m];
-// 		visited[0][0] = true;
-// 		bfs(0, 0);
-// 		System.out.println(map[n-1][m-1]);
-// 	}
-
-// 	public static void bfs(int x, int y) {
-// 		Queue<int[]> q = new LinkedList<>();
-// 		q.add(new int[] {x,y});
-
-// 		while(!q.isEmpty()) {
-// 			int now[] = q.poll();
-// 			int nowX = now[0];
-// 			int nowY = now[1];
-// 			System.out.println(Arrays.toString(now));
-// 			for(int i=0; i<4; i++) {
-// 				int nextX = nowX + dx[i];
-// 				int nextY = nowY + dy[i];
-//                 System.out.println("nextX :"+nextX);
-//                 System.out.println("nextY :"+nextY);
-				
-// 		                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m)
-//                 		    continue;
-//         		        if (visited[nextX][nextY] || map[nextX][nextY] == 0)
-//                     		continue;
-                    
-// 		                q.add(new int[] {nextX, nextY});
-//         		        map[nextX][nextY] = map[nowX][nowY] + 1;
-//                 		visited[nextX][nextY] = true;
-//                         System.out.println("visitied : "+visited[nextX][nextY]);
-//                         System.out.println(map[nextX][nextY]);
-// 			}
-// 		}
-// 	}
-// }
-
-
 import java.util.*;
 import java.io.*;
 
@@ -70,7 +7,6 @@ class B_2178{
     static int M;
     static int[][] arr;
     static boolean[][] visited;
-    static int cnt = 0;
     static int[] dx = { -1, 1, 0, 0 };
     static int[] dy = { 0, 0, -1, 1 };
     public static void main(String[] args)throws IOException{
@@ -105,7 +41,7 @@ class B_2178{
 
 		while(!q.isEmpty()) {
             
-			int now[] = q.poll();
+			int[] now = q.poll();  //??????
 			int nowX = now[0];
 			int nowY = now[1];
 		
@@ -114,16 +50,17 @@ class B_2178{
 				int nextY = nowY + dy[i];
 
 				
-		                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M)
+		                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) //움직일 수 있는 범위 제한 및 체크
                 		    continue;
-        		        if (visited[nextX][nextY] || arr[nextX][nextY] == 0)
+        		        if (visited[nextX][nextY] || arr[nextX][nextY] == 0) // 방문여부와 지나갈 수 있는 길인지 체크
                     		continue;
   
-		                q.add(new int[] {nextX, nextY});
-        		        arr[nextX][nextY] = arr[nowX][nowY] + 1;
-                		visited[nextX][nextY] = true;
+		                q.add(new int[] {nextX, nextY}); //모든 조건을 통과할 경우 q에 이동할 수 있는 지점을 저장
+        		        arr[nextX][nextY] = arr[nowX][nowY] + 1; //이전 지점 + 1의 값을 이동한 배열에 저장하여 움직인 거리를 축적한다.
+                		visited[nextX][nextY] = true; //방문 체크
 
 			}
 		}
     }
 }
+
